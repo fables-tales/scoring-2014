74bcf97ecaa1271241839649fdeb1f5a30bf839a
Peter Law <PeterJCLaw@gmail.com>
2014-03-01 23:08:35 +0000

    Add testing via input and output YAML files.
    This makes adding cli level tests as easy as definig the inputs
    and outputs.

--- test/cli_test.py
+++ test/cli_test.py
@@ -1,3 +1,5 @@
+
+import os
 import subprocess
 import yaml
 
@@ -9,17 +11,25 @@ def tla_result_fixture(zone_number):
         "zone": zone_number,
     }
 
-def test_run_the_template():
-    process = subprocess.Popen(["./score.py",  "templates/yaml_template.yaml"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def run(relative_path):
+    process = subprocess.Popen(["./score.py",  relative_path], \
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     retcode = process.wait()
+    return retcode, process
+
+def assert_run(relative_path):
+    retcode, process = run(relative_path)
     if retcode != 0:
         print process.stderr.read()
 
-    assert retcode == 0
+    assert retcode == 0, "Bad return code scoring '{0}'.".format(relative_path)
 
     result = process.stdout.reads()
     result_dict = yaml.load(result)
+    return result_dict
 
+def test_run_the_template():
+    result_dict = assert_run("templates/yaml_template.yaml")
 
     assert result_dict["version"] == "1.0.0"
     assert result_dict["match_number"] == 0
@@ -29,3 +39,22 @@ def test_run_the_template():
         "TLA3": tla_result_fixture(2),
         "TLA4": tla_result_fixture(3),
     }
+
+def check_by_input_file(input_name):
+    input_file = os.path.join("test/data", input_name)
+    output_file = os.path.join("test/data", input_name[:-5] + '.out.yaml')
+
+    assert os.path.exists(output_file), "Missing output expectation '{1}' for input '{0}'.".format(input_name, output_file)
+
+    expected_output = yaml.load(open(output_file).read())
+    output = assert_run(input_file)
+
+    assert output == expected_output
+
+def test_input_file():
+    files = os.listdir("test/data")
+    outputs = [f for f in files if f.endswith('.out.yaml')]
+    inputs = [f for f in files if f.endswith('.yaml') and not f in outputs]
+
+    for input_name in inputs:
+        yield check_by_input_file, input_name
