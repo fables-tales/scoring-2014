c8c9df9966eabfff09fbc04eec540c44757666d0
Peter Law <PeterJCLaw@gmail.com>
2014-03-02 14:22:36 +0000

    Implement reformatting the scoresheet.
    The input format is designed to be easy to check against the paper
    forms we'll be filling in, and isn't very useful for processing.
    Add a step which converts the input to a more useful layout.
    
    This includes some placeholder functions which will contain the body
    of the scoring logic.

--- test/scorer_test.py
+++ test/scorer_test.py
@@ -1,4 +1,5 @@
 
+import mock
 import os
 import sys
 import yaml
@@ -27,3 +28,67 @@ def test_input_file():
 
     for input_name in inputs:
         yield check_by_input_file, input_name
+
+def test_isolated_scores():
+    mock_tidy_zones = mock.Mock()
+    mock_tidy_slots = mock.Mock()
+
+    zones_1 = 'zones_1'
+    zones_2 = 'zones_2'
+    slots_1 = 'slots_1'
+    slots_2 = 'slots_2'
+
+    zones_1_out = 'zones_1_out'
+    zones_2_out = 'zones_2_out'
+    slots_1_out = 'slots_1_out'
+    slots_2_out = 'slots_2_out'
+
+    input_ = {
+        "TLA1": {
+            "robot_moved": False,
+            "zone_tokens": zones_1,
+            "slot_bottoms": slots_1,
+            "upright_tokens": 0,
+        },
+        "TLA2": {
+            "robot_moved": True,
+            "zone_tokens": zones_2,
+            "slot_bottoms": slots_2,
+            "upright_tokens": 2,
+        },
+    }
+
+    expected_zones = { "TLA1": zones_1, "TLA2": zones_2 }
+    expected_slots = { "TLA1": slots_1, "TLA2": slots_2 }
+
+    expected_zones_out = { "TLA1": zones_1_out, "TLA2": zones_2_out }
+    expected_slots_out = { "TLA1": slots_1_out, "TLA2": slots_2_out }
+
+    mock_tidy_zones.return_value = expected_zones_out
+    mock_tidy_slots.return_value = expected_slots_out
+
+    expected = {
+        "TLA1": {
+            "robot_moved": False,
+            "zones_owned": zones_1_out,
+            "slots_owned": slots_1_out,
+            "upright_tokens": 0,
+        },
+        "TLA2": {
+            "robot_moved": True,
+            "zones_owned": zones_2_out,
+            "slots_owned": slots_2_out,
+            "upright_tokens": 2,
+        },
+    }
+
+    with mock.patch('scorer.tidy_zones', mock_tidy_zones), \
+            mock.patch('scorer.tidy_slots', mock_tidy_slots):
+
+        scorer = Scorer(input_)
+        actual = scorer.isolated_scores
+
+        mock_tidy_zones.assert_called_once_with(expected_zones)
+        mock_tidy_slots.assert_called_once_with(expected_slots)
+
+        assert actual == expected, "Wrong isolated scores."
